1e6d05afede19253b1f892280473b4e3badc233f
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-11-02 21:48:32 +0100

    Comments

--- src/shortener/logic.go
+++ src/shortener/logic.go
@@ -17,17 +17,21 @@ type redirectService struct {
 	redirectRepo RedirectRepository
 }
 
+/** Creates a Redirect service */
 func NewRedirectService(redirectRepo RedirectRepository) RedirectService {
 	return &redirectService{
 		redirectRepo,
 	}
 }
 
+/** Given a code returns a Redirect */
 func (r *redirectService) Find(code string) (*Redirect, error) {
 	return r.redirectRepo.Find(code)
 }
 
+/** Creates a code for a given URL */
 func (r *redirectService) Store(redirect *Redirect) error {
+	// Check it is in URL format and not empty
 	if err := validate.Validate(redirect); err != nil {
 		return errs.Wrap(ErrRedirectInvalid, "service.Redirect.Store")
 	}
