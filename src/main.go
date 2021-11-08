1a33b77cb8e8c2917210ce2599f85247f76b8f6e
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-11-08 19:58:27 +0100

    Port 8000

--- src/main.go
+++ src/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 
+}
+
+/** Port in 8000*/
+func HttpPort() string {
+	port := "8000"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	return fmt.Sprintf(":%s", port)
 }
