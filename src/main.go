87d291ff75c2f76a767872d41de073e948dd12cf
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-11-08 20:01:41 +0100

    Type of db checker

--- src/main.go
+++ src/main.go
@@ -3,6 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
+	"strconv"
+
+	mongo "github.com/lopdan/url-shortener/src/repository/mongodb"
+	redis "github.com/lopdan/url-shortener/src/repository/redis"
+	"github.com/lopdan/url-shortener/src/shortener"
 )
 
 func main() {
@@ -16,4 +22,29 @@ func HttpPort() string {
 		port = os.Getenv("PORT")
 	}
 	return fmt.Sprintf(":%s", port)
+}
+
+/** Check type of database */
+func ChooseRepo() shortener.RedirectRepository {
+	switch os.Getenv("URL_DB") {
+	case "redis":
+		// Look for enviroment variable 
+		redisURL := os.Getenv("REDIS_URL")
+		repo, err := redis.NewRedisRepository(redisURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	case "mongo":
+		// Look for enviroment variables 
+		mongoURL := os.Getenv("MONGO_URL")
+		mongodb := os.Getenv("MONGO_DB")
+		mongoTimeout, _ := strconv.Atoi(os.Getenv("MONGO_TIMEOUT"))
+		repo, err := mongo.NewMongoRepository(mongoURL, mongodb, mongoTimeout)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	}
+	return nil
 }
